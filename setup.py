786cea53b97b25ef1f6bb996c37baae48859b180
yeshu999 <vootlayeswanth20@gmail.com>
2025-08-11 14:47:55 -0400

    version 0.2.6

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ ext_modules = [
 
 setup(
     name="effspm",
-    version="0.2.5",
+    version="0.2.6",
     description="Efficient Sequential Pattern Mining Library",
     author="Yeswanth Vootla",
     packages=["effspm"],
